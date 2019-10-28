2624838585f99535053d4691235ae4a2d4e05b0c
Adam Davies <45388618+Alvin76@users.noreply.github.com>
2019-10-27 20:09:12 -0400

    Ajustments to css

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Project3
  Making a comic website with NodeJS
 
-link to website on Heroku: 
+link to website on Heroku: https://adams-nodejs-assignment3.herokuapp.com/
 
 This is the JS version the PHP website that I should have made work and didn't hopefully this will go better.
--- app.js
+++ app.js
@@ -5,8 +5,8 @@ const router = express.Router();
 const axios = require("axios");
 var bodyParser = require("body-parser");
 const app = express();
-const port = process.env.PORT || 3000;
-console.log("Starting server on port 3000")
+//const port = process.env.PORT || 3000;
+//console.log("Starting server on port 3000")
 app.set('views', path.join(__dirname, 'views'));
 app.set("view engine", "ejs");
 app.use(bodyParser.json());
